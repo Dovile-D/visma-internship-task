84860d3e0b3bfbfd7984e4ab8b663d2f8d332ac2
Dovile-D <dovile.d.git@gmail.com>
2022-06-20 14:58:53 +0300

    add Meeting

--- src/main/java/com/dovdau/vismatask/models/Meeting.java
+++ src/main/java/com/dovdau/vismatask/models/Meeting.java
@@ -1,4 +1,26 @@
 package com.dovdau.vismatask.models;
 
+import com.dovdau.vismatask.enums.Category;
+import com.dovdau.vismatask.enums.Type;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Meeting {
+    private int id;
+    private String name;
+    private Person responsiblePerson;
+    private String description;
+    private Category category;
+    private Type type;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private ArrayList<Person> participants;
+
 }
