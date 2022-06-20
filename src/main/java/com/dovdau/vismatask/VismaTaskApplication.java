9441070b54617446850c07748e8e8785012f4d8a
Dovile-D <dovile.d.git@gmail.com>
2022-06-20 14:41:28 +0300

    initial commit

---
+++ src/main/java/com/dovdau/vismatask/VismaTaskApplication.java
@@ -0,0 +1,13 @@
+package com.dovdau.vismatask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VismaTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VismaTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dovdau/vismatask/VismaTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dovdau.vismatask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VismaTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
